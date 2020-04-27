4d2cb337968fca30c1eed42286fa1ec6e87478c0
emi10860 <echeroske@esri.com>
2020-04-26 22:07:23 -0400

    Tring to optimize this gradient path code a little bit more - instead of drawing a gradient between each recorded point, we draw a gradient along a Bezier curve between every 10th points to reduce the number of paths being redered on the map. This speeds things up a bit and makes it a bit smoother. Might up this number even more? Still need to solve the chunkly line endings to smooth out the route even more.

--- Workouts/HelpersAndExtensions/GradientPolyline.swift
+++ Workouts/HelpersAndExtensions/GradientPolyline.swift
@@ -14,7 +14,10 @@ import MapKit
 // so that whatever route you are looking at - the speed is relative to itself. With the static values - a walk will show up all red, and a run will show up all green, we really want to compare it against itself
 // when making these assumptions about how fast you are going. Also - instead of doing a bunch of checks (the example I based this off of on Stack Overflow was doing a ton of if vmax... else blah blah blah)
 // We can make this code better by making color a function of where the velocity falls in the range between the min/max velocities of the entire route.
-//
+// Other updates! Instead of drawing gradient path between every single point - we instead draw lines between every 10th
+// (maybe this should be a funciton of the zoom level of the map?) point and use the middle point in between the start and
+// end of the path as a control for a Bezier curve (this let's us 'fake' the curve a user would have followed to get from
+// point a to b without us actually having to draw all of that data on the map - really speeds things up)
 class GradientPolyline: MKPolyline {
     var colors: [UIColor]?
     public func getColor(from index: Int) -> CGColor {
@@ -36,7 +39,7 @@ extension GradientPolyline {
 
             // hue from 0.03 to ~.3 is perfect for the red to green transition
             //
-            return UIColor(hue: (velocityIntentsity * 0.3) + 0.03, saturation: 1, brightness: 1, alpha: 1)
+            return UIColor(hue: (velocityIntentsity * 0.3) + 0.03, saturation: 1, brightness: 1, alpha: 0.8)
         })
     }
 }
@@ -54,7 +57,9 @@ class GradidentPolylineRenderer: MKPolylineRenderer {
 
         guard let polyLine = self.polyline as? GradientPolyline else { return }
 
-        for index in 0...self.polyline.pointCount - 1{
+        let stepCount = 10 // <-- make this a funciton of the zoom level? This allows us to save 10x fewer paths on the map which is just a constant, but it makes it smoother.
+        
+        for index in stride(from: 0, to: self.polyline.pointCount - stepCount, by: stepCount) {
             let point = self.point(for: self.polyline.points()[index])
             let path = CGMutablePath()
 
@@ -63,19 +68,26 @@ class GradidentPolylineRenderer: MKPolylineRenderer {
             if index == 0 {
                path.move(to: point)
             } else {
-                let prevPoint = self.point(for: self.polyline.points()[index - 1])
+                let prevPoint = self.point(for: self.polyline.points()[index - stepCount])
+                let controlPoint = self.point(for: self.polyline.points()[index - (stepCount/2)])
                 path.move(to: prevPoint)
-                path.addLine(to: point)
-
+                
+                // Bezier curve between prevPoint and current point,
+                // we take the point in between then to use as a control that lets us see approximately where the user
+                // would have walked. https://developer.apple.com/documentation/uikit/uibezierpath/1624351-addquadcurve
+                //
+                path.addQuadCurve(to: point, control: controlPoint)
+                
                 let colors = [prevColor!, currentColor!] as CFArray
                 let baseWidth = self.lineWidth / zoomScale
-
+                
                 context.saveGState()
                 context.addPath(path)
 
                 let gradient = CGGradient(colorsSpace: nil, colors: colors, locations: [0, 1])
 
                 context.setLineWidth(baseWidth)
+                
                 context.replacePathWithStrokedPath()
                 context.clip()
                 context.drawLinearGradient(gradient!, start: prevPoint, end: point, options: [])
---
+++ Workouts/SupportingViews/SharedComponents/DailyWorkout.swift
@@ -0,0 +1,21 @@
+//
+//  DailyWorkout.swift
+//  Workouts
+//
+//  Created by Emily Cheroske on 4/25/20.
+//  Copyright © 2020 Dawit Elias. All rights reserved.
+//
+
+import SwiftUI
+
+struct DailyWorkout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DailyWorkout_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyWorkout()
+    }
+}
