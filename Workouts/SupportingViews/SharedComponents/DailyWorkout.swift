a64a19e1646cf90246ba726c7eaabe7279b795d3
emi10860 <echeroske@esri.com>
2020-05-07 19:11:41 -0400

    Updates to the daily workout card (the card that shows up if you've done more than one workout in a day)

--- Workouts/SupportingViews/SharedComponents/DailyWorkout.swift
+++ Workouts/SupportingViews/SharedComponents/DailyWorkout.swift
@@ -13,39 +13,43 @@ struct DailyWorkout: View {
     var workout: HKWorkout
 
     var body: some View {
-        let workoutDistance = workout.totalDistance?.doubleValue(for: .mile()) ?? 0
-
-        let distanceString = "\(String.init(format: "%.0f", workoutDistance)) mile"
         let workoutHrAndMin = workout.duration.getHoursAndMinutesString()
 
         return HStack {
-            VStack(alignment: .leading, spacing: 5) {
-                Text(workout.workoutActivityType.workoutTypeMetadata.activityTypeDescription)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(workout.workoutActivityType.workoutTypeMetadata.highlightColor)
+            ZStack(alignment: .topLeading) {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(workout.workoutActivityType.workoutTypeMetadata.activityTypeDescription)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(workout.workoutActivityType.workoutTypeMetadata.highlightColor)
+                        .fixedSize()
 
-                Text("\(workoutHrAndMin)")
-                    .font(.headline)
-                    .foregroundColor(Color(UIColor.label))
-                
-                if workout.totalEnergyBurned != nil {
-                    Text("\(workout.totalEnergyBurned!.doubleValue(for: .kilocalorie())) cal")
-                        .font(.headline)
+                    Text("\(workoutHrAndMin)")
+                        .font(.title)
                         .foregroundColor(Color(UIColor.label))
                     
+                    if workout.totalEnergyBurned != nil {
+                        Text("\(Int(workout.totalEnergyBurned!.doubleValue(for: .kilocalorie()))) cal")
+                            .font(.title)
+                            .foregroundColor(Color(UIColor.label))
+                        
+                    }
+                    
+                    
                 }
+                .frame(width: 200, height: nil, alignment: .leading)
+                .padding(.leading, 20)
+                .padding(.top, 20)
                 
-                
+                VStack(alignment: .trailing, spacing: nil) {
+                    Image(workout.workoutActivityType.workoutTypeMetadata.systemIconName)
+                        .foregroundColor(workout.workoutActivityType.workoutTypeMetadata.highlightColor)
+                        .opacity(0.2)
+                }.offset(x: 70, y: 0)
             }
-            .padding()
-            .frame(width: 200, height: nil, alignment: .center)
+            
         }
-        .background(LinearGradient(
-            gradient: .init(colors: [workout.workoutActivityType.workoutTypeMetadata.mainColor, workout.workoutActivityType.workoutTypeMetadata.highlightColor]),
-            startPoint: .init(x: 0.0, y: 0.0),
-            endPoint: .init(x: 0.1, y: 1)
-        ))
+        .background(Color(UIColor.tertiarySystemFill))
         .cornerRadius(5)
     }
 }
