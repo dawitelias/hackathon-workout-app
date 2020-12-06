9a9ba159b2a3119cfd2d0df09b63fd6d03ef5b5a
emi10860 <echeroske@esri.com>
2020-12-05 22:30:01 -0500

    More cleanup

--- Workouts/SupportingViews/FilterScreen/WorkoutsMultiSelect.swift
+++ Workouts/SupportingViews/FilterScreen/WorkoutsMultiSelect.swift
@@ -9,32 +9,68 @@
 import SwiftUI
 
 struct WorkoutsMultiSelect: View {
+
     @EnvironmentObject var workoutData: WorkoutData
 
     var body: some View {
+
         List {
-            ForEach(self.workoutData.activityTypeFilters, id: \.self) { item in
+
+            ForEach(workoutData.activityTypeFilters, id: \.self) { item in
+
                 Button(action: {
-                    self.workoutData.toggleActivityFilterApplied(filter: item)
+
+                    workoutData.toggleActivityFilterApplied(filter: item)
+
                 }) {
+
                     HStack {
+
                         Text(item.value.workoutTypeMetadata.activityTypeDescription)
                             .foregroundColor(Color(UIColor.label))
+
                         Spacer()
+
                         if item.isApplied {
-                            Image(systemName: "checkmark")
+
+                            Image(systemName: Images.checkmark)
                                 .accentColor(/*@START_MENU_TOKEN@*/.pink/*@END_MENU_TOKEN@*/)
+
                         }
+
                     }
+
                 }
+
             }
+
         }
         .modifier(GroupedListModifier())
-        .navigationBarTitle("Workout Types")
+        .navigationBarTitle(Strings.workoutTypes)
     }
+
 }
 
-struct MultiSelect_Previews: PreviewProvider {
+// MARK: Assets and Strings
+//
+extension WorkoutsMultiSelect {
+    
+    private enum Images: String {
+        case checkmark
+    }
+    
+    private struct Strings {
+        
+        static var workoutTypes: String {
+            NSLocalizedString("com.okapi.workoutsMultiSelect.title", value: "Workout Types", comment: "Title for modal allowing user to select multiple types of workouts")
+        }
+    }
+
+}
+
+// MARK: Previews
+//
+struct WorkoutsMultiSelect_Previews: PreviewProvider {
     static var previews: some View {
         WorkoutsMultiSelect().environmentObject(WorkoutData())
     }
