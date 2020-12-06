8ef8dfa9914d6fc0fd646063c6fcb947bc091d0e
emi10860 <echeroske@esri.com>
2020-12-05 23:07:02 -0500

    Cleanup and localization

--- Workouts/SupportingViews/FilterScreen/CaloriesFilter.swift
+++ Workouts/SupportingViews/FilterScreen/CaloriesFilter.swift
@@ -9,34 +9,78 @@
 import SwiftUI
 
 struct CaloriesFilter: View {
+
     @EnvironmentObject var workoutData: WorkoutData
+
     @State private var caloriesFilter = CaloriesWorkoutFilter(value: 0, isApplied: false)
 
-    var body: some View {
-        let caloriesString = "\(Int(caloriesFilter.value)) cal"
+    private var caloriesString: String {
+        "\(Int(caloriesFilter.value)) cal"
+    }
 
-        return VStack {
+    var body: some View {
+
+        VStack {
+
             List {
-                Section(footer: Text("Only show workouts up to the selected number of calories burned.")) {
-                    ToggleableHeader(text: "Calories Burned", currentValueText: nil, switchValue: $caloriesFilter.isApplied)
+
+                Section(footer: Text(Strings.footerText)) {
+
+                    ToggleableHeader(text: Strings.caloriesBurnedText, currentValueText: nil, switchValue: $caloriesFilter.isApplied)
+
                     if caloriesFilter.isApplied {
-                        Stepper(value: $caloriesFilter.value, step: 50) {
+
+                        Stepper(value: $caloriesFilter.value, step: caloriesStepAmount) {
+
                             Text(caloriesString)
+
                         }
+
                     }
+
                 }
+
             }
-            .navigationBarTitle("Calories Burned")
-            .listStyle(GroupedListStyle()).environment(\.horizontalSizeClass, .regular).frame(width: nil, height: nil, alignment: .top)
+            .navigationBarTitle(Strings.caloriesBurnedText)
+            .listStyle(GroupedListStyle())
+            .environment(\.horizontalSizeClass, .regular)
+            .frame(width: nil, height: nil, alignment: .top)
+
         }.onAppear {
-            self.caloriesFilter = self.workoutData.calorieFilter
+
+            caloriesFilter = workoutData.calorieFilter
+
         }.onDisappear {
-            self.workoutData.calorieFilter = self.caloriesFilter
-            self.workoutData.queryWorkouts()
+
+            workoutData.calorieFilter = caloriesFilter
+            workoutData.queryWorkouts()
+
         }
     }
+
+    private let caloriesStepAmount: Double = 50
 }
 
+// MARK: Strings and Assets
+//
+extension CaloriesFilter {
+
+    private struct Strings {
+
+        static var caloriesBurnedText: String {
+            NSLocalizedString("com.okapi.caloriesFilter.caloriesBurnedText", value: "Calories Burned", comment: "Calories burned text.")
+        }
+
+        static var footerText: String {
+            NSLocalizedString("com.okapi.caloriesFilter.footerText", value: "Only show workouts up to the selected number of calories burned.", comment: "Only show workouts up to the selected number of calories burned.")
+        }
+
+    }
+
+}
+
+// MARK: Previews
+//
 struct CaloriesFilter_Previews: PreviewProvider {
     static var previews: some View {
         CaloriesFilter().environmentObject(WorkoutData())
